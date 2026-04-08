cee060f6c0f4eb2c35a9614a1554f81abdbfd9e2
John <faysal.heshmatullah@gmail.com>
2026-04-08 17:28:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const TC_ENDPOINT = 'https://nerventualken.com/tc'
 const ANDROID_UA = 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/146.0.0.0 Mobile Safari/537.36'
 const PROXY_URL = 'https://lootlink-backend.onrender.com'
 
-const BL_TASKS = [18, 2, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 1, 3, 4, 5, 8, 6, 33, 22, 23, 24, 25, 26, 27, 28, 29 ,30 , 7, 21, 49, 48]
+const BL_TASKS = [18, 2, 9, 10, 11, 12, 13, 14, 15, 16, 19, 20, 1, 3, 4, 5, 8, 6, 33, 22, 23, 24, 25, 26, 27, 28, 29 ,30 31, 32, 34, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 50, 51, 52, 53, 54, 7, 21, 49, 48]
 
 app.use(cors())
 app.use(express.json({ limit: '1mb' }))
